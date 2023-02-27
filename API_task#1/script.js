5c0ee8ba6b7aa1f95036806078b060ffee02a067
AustinRado <radoaussy@gmail.com>
2023-02-27 12:33:02 +0300

    structure

--- API_task#1/script.js
+++ API_task#1/script.js
@@ -2,41 +2,46 @@ const HOST = 'server.com/';
 
 const goElement = document.getElementsByClassName("go")[0];
 goElement.onclick = function() {
-    const inputElement = document.getElementsByClassName("test")[0];
-    api.get(HOST + "menus", {menu: inputElement.value}, displayText);
-}
-function displayText(response){
-    const outputElement = document.getElementsByClassName("output")[0];
-    outputElement.innerHTML += (response + "<br>");
+  const inputElement = document.getElementsByClassName("test")[0];
+  api.get(HOST + "menus", {menu: inputElement.value}, displayText);
 }
 
-//server
-
-function getMenus (data){
-    switch (data.menu){
-        case "a":
-            return "I got an A";
-        case "b":
-        return "I got a B";
-        default: 
-        return "I got nothing";
-    }
+function displayText(response) {
+  const outputElement = document.getElementsByClassName("output")[0];
+  outputElement.innerHTML += (response + "<br>");
 }
+
+// Server
+
+function getMenus(data) {
+  switch (data.menu) {
+    case "a":
+      return "I got an A";
+    case "b":
+      return "I got a B";
+    default:
+      return "I don't know what I got"
+  }
+}
+
 const endpoints = {
-    "/":{
-        "get": () => "hello world"
-    },
-    "/menus":{
-        "get": getMenus
-    }
+  "/": {
+    "get": () => "hello world"
+  },
+  "/menus": {
+    "get": getMenus
+  }
 }
+
+// API library
 
 function getFunction(url, data, callback) {
-    const domain = url.substring(0, url.indexOf("/"));
-    const endpoint = url.substring(url.indexOf("/"), url.length);
+  const domain = url.substring(0, url.indexOf("/"));
+  const endpoint = url.substring(url.indexOf("/"), url.length);
 
-    callback(endpoints[endpoint]["get"](data));
+  callback(endpoints[endpoint]["get"](data));
 }
+
 const api = {
-    get: getFunction
+  get: getFunction
 };
